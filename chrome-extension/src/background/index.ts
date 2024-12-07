679754ecdf767604ab2a33751be14ab707f74a04
Joseph Chiang <joseph.chiang@ignitionapp.com>
2024-12-07 20:49:16 +0000

    Update basic files

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -1,9 +1,7 @@
 import 'webextension-polyfill';
 import { exampleThemeStorage } from '@extension/storage';
+// chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 
 exampleThemeStorage.get().then(theme => {
   console.log('theme', theme);
 });
-
-console.log('background loaded');
-console.log("Edit 'chrome-extension/src/background/index.ts' and save to reload.");
